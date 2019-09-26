5b79db14a7ab96051f030b16a55ad0362eaf6dc9
Dani Gábor <35999247+JaskierDan@users.noreply.github.com>
2019-09-26 11:57:38 +0200

    Create Program.cs

---
+++ 14. SZ/Lessons/2019.09.26./Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogaGyak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mammal m = new Mammal("Kutya");
+            m.ShowName();
+        }
+    }
+}
