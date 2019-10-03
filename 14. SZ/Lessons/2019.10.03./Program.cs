3f6f7d487a11da342affbad53c548cc78b5c94ae
Dani Gábor <35999247+JaskierDan@users.noreply.github.com>
2019-10-03 11:10:11 +0200

    Update Program.cs

--- 14. SZ/Lessons/2019.10.03./Program.cs
+++ 14. SZ/Lessons/2019.10.03./Program.cs
@@ -5,20 +5,38 @@ using System.Security.Cryptography;
 namespace SecuritySystem
 {
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
             Connection conn = new Connection();
             conn.Connect();
-            Console.Write("Password: ");
-            string pwd = SecurityBase.Encrypt(Console.ReadLine());
-            SecurityBase sb = new SecurityBase(pwd);
-            Console.WriteLine(sb.ToString());
-            Console.Write("New User: ");
-            string user = Console.ReadLine();
-            Console.Write("Password: ");
-            string npwd = SecurityBase.Encrypt(Console.ReadLine());
-            conn.CreateAccount(user, npwd);
+            string cmd = null, user = null, pwd = null;
+            while (cmd != "3")
+            {
+                Console.WriteLine("1 - Create Account\n2 - Login\n3 - Exit");
+                cmd = Console.ReadLine();
+                switch (cmd)
+                {
+                    case "1":
+                        Console.WriteLine("CREATE ACCOUNT");
+                        Console.Write("New User: ");
+                        user = Console.ReadLine();
+                        Console.Write("Password: ");
+                        pwd = SecurityBase.Encrypt(Console.ReadLine());
+                        conn.CreateAccount(user, pwd);
+                        break;
+                    case "2":
+                        Console.WriteLine("LOGIN");
+                        Console.Write("Username: ");
+                        user = Console.ReadLine();
+                        Console.Write("Password: ");
+                        pwd = SecurityBase.Encrypt(Console.ReadLine());
+                        conn.CheckUser(user, pwd);
+                        break;
+                    default:
+                        break;
+                }
+            }
             conn.CloseConnection();
         }
     }
