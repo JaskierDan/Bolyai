fbbc7d6b381314a3a79ca094b6ab333521b83c1d
Dani Gábor <35999247+JaskierDan@users.noreply.github.com>
2019-11-14 08:45:17 +0100

    Create Person.cs

---
+++ 14. SZ/Lessons/2019.11.14./Person.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumPractice
+{
+    abstract class Person
+    {
+        public virtual void GetAllValues(List<string> workDays) { }
+    }
+}
